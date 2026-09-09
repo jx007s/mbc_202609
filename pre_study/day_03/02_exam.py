f77166acfd66b33878a5410ebf8e1a0d1252f11b
nemo <jx007s@gmail.com>
2026-09-09 11:42:30 +0900

    9_9_2

---
+++ pre_study/day_03/02_exam.py
@@ -0,0 +1,13 @@
+name = "장동건"
+kor = 88
+eng = 86
+mat = 82
+tot = kor + eng + mat
+avg = tot / 3
+print("이름 : ", name)
+print("국어 : ", kor)
+print("영어 : ", eng)
+print("수학 : ", mat)
+print("총점 : ", tot)
+print("평균 : ", avg)
+#평균2 :  85.33
